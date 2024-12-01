3ac53b8cdc4fb13dd36bd0227949a8ba06bbd8a9
ceydabiber <ceydabiber3@gmail.com>
2024-12-01 19:51:58 +0300

    created a queue

--- quiz-app/Unit1.cpp
+++ quiz-app/Unit1.cpp
@@ -34,7 +34,15 @@ std::queue<Question> LoadQuestions(){
 
 	Question q1= Question(" Avustralya'nýn baþkenti neresidir?","Sydney","Melbourne","Canberra", 3);
 	Question q2= Question(" Kýzýl Gezegen olarak bilinen gezegen hangisidir?","Merkür","Mars","Jüpiter", 2);
-}   Question q3= Question(" Kanada'nýn baþkenti neresidir?","Ottawa","Toronto","Vancouver", 1);
+	Question q3= Question(" Kanada'nýn baþkenti neresidir?","Ottawa","Toronto","Vancouver", 1);
+
+	std::queue<Question> questions;
+	questions.push(q1);
+	questions.push(q2);
+	questions.push(q3);
+
+	return questions;
+	}
 TForm1 *Form1;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
